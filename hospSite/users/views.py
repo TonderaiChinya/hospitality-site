090a7669e7a863a9bd31aec29ea138d1d2398dc0
tonderai chinya <tonderaiclemzc5@gmail.com>
2020-06-15 18:09:09 +0200

    added registration form

--- hospSite/users/views.py
+++ hospSite/users/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
-def user(request):
-    return HttpResponse('<h1>My Name is Tonderai Chinya</h1>')
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('blog-home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})

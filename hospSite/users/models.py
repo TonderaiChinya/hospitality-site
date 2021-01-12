e05d9dc3a93297a44e0e33a30be6b7ebb3690eda
tonderai chinya <tonderaiclemzc5@gmail.com>
2021-01-12 16:52:36 +0200

    new additions

--- hospSite/users/models.py
+++ hospSite/users/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime, date
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpeg', upload_to='profile_pics')
+    date_of_birth = models.DateField(auto_now_add=False, auto_now=False, blank=True)
+    location = models.CharField(max_length=50)
 
     def __str__(self):
         return f'{self.user.username} Profile'
